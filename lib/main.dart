c239ed56f32e572bbe93439e993fd631b238ee59
abhijithgkrishna <a4abhijithgkrishna@gmail.com>
2022-11-02 23:04:47 +0530

    changed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to MyApp',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to world'),
+          title: const Text('Start-up Name Generator'),
           backgroundColor: const Color.fromARGB(229, 41, 174, 63),
         ),
         body: const Center(
